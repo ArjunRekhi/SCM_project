7d31698144add4a02b943f7b6674f260394c6c91
ArjunRekhi <arjunrekhi265@gmail.com>
2019-04-21 21:57:27 +0530

    name of the branches in the college and the code for librarian added

--- management.cpp
+++ management.cpp
@@ -51,3 +51,73 @@ class Lib
           void fine(int,int,int,int,int,int);
           
 };
+
+void Lib::librarian()
+{
+    int i;
+        cout<<"\n\t************ WELCOME LIBRARIAN ************\n";
+        cout<<"\n\t\t>>Please Choose One Option:\n";
+        cout<<"\n\t\t1.View BookList\n\n\t\t2.Search for a Book\n\n\t\t3.Modify/Add Book\n\n\t\t4.Issue Book\n\n\t\t5.Go to main menu\n\n\t\t6.Change Password\n\n\t\t7.Close Application\n";
+        cout<<"\n\t\tEnter your choice : ";
+        cin>>i;
+        switch(i)
+        {
+
+            case 1:booklist(2);
+                     break;
+            case 2:see(2);
+                     break;
+            case 3:modify();
+                     break;
+            case 4:issue();
+                     break;
+            case 5:system("cls");
+                     get();
+                     break;
+            case 6:password();
+                    break;
+            case 7:exit(0);
+            default:cout<<"\n\t\tPlease enter correct option :(";
+            getch();
+            system("cls");
+            librarian();
+
+        }
+
+}
+~
+~
+int Lib::branch(int x)
+  {
+      int i;
+      cout<<"\n\t\t>>Please Choose one Branch :-\n";
+      cout<<"\n\t\t1.BIT\n\n\t\t2.EE\n\n\t\t3.EC\n\n\t\t4.CIVIL\n\n\t\t5.MECHANICAL\n\n\t\t6.1ST YEAR\n\n\t\t7.Go to menu\n";
+      cout<<"\n\t\tEnter youur choice : ";
+      cin>>i;
+      switch(i)
+      {
+          case 1: return 1;
+                  break;
+          case 2: return 2;
+                  break;
+          case 3: return 3;
+                  break;
+          case 4: return 4;
+                  break;
+          case 5: return 5;
+                  break;
+          case 6: return 6;
+                  break;
+          case 7: system("cls");
+                  if(x==1)
+                  student();
+                  else
+                    librarian();
+          default : cout<<"\n\t\tPlease enter correct option :(";
+                    getch();
+                    system("cls");
+                    branch(x);
+        }
+  }~
+~
+
