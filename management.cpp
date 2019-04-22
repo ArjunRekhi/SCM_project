e8222ac1aac7153813964a7f39a439bbadad9784
Ajay Tyagi <ajaytyagi0209@gmail.com>
2019-04-22 23:38:04 +0530

    modify functionality,search by book name added

--- management.cpp
+++ management.cpp
@@ -120,4 +120,64 @@ int Lib::branch(int x)
         }
   }~
 ~
-
+void Lib::modify()
+  {
+  	
+    char ch,st1[100];
+    int i=0,b,cont=0;
+    system("cls");
+    cout<<"\n\t\t>>Please Choose one option :-\n";
+    cout<<"\n\t\t1.Modification In Current Books\n\n\t\t2.Add New Book\n\n\t\t3.Delete A Book\n\n\t\t4.Go back\n";
+    cout<<"\n\n\t\tEnter your choice : ";
+    cin>>i;
+    if(i==1)
+    {
+    	
+                    system("cls");
+                    b=branch(2);
+                    ifstream intf1("Booksdata.txt",ios::binary);
+                    if(!intf1)
+                    {
+                        cout<<"\n\t\tFile Not Found\n";
+                        cout<<"\n\t\tPress any key to continue.....";
+                        getch();
+                        system("cls");
+                        librarian();
+                    }
+                        intf1.close();
+                        system("cls");
+                        cout<<"\n\t\tPlease Choose One Option :-\n";
+                        cout<<"\n\t\t1.Search By Book Name\n\n\t\t2.Search By Book's ID\n";
+                        cout<<"\n\t\tEnter Your Choice : ";
+                        cin>>i;
+                        fflush(stdin);
+                        if(i==1)
+                            {
+                                system("cls");
+                                cout<<"\n\t\tEnter Book Name : ";
+                                cin.getline(st1,100);
+                                system("cls");
+                                fstream intf("Booksdata.txt",ios::in|ios::out|ios::ate|ios::binary);
+                                intf.seekg(0);
+                                intf.read((char*)this,sizeof(*this));
+                                while(!intf.eof())
+                                    {
+                                        for(i=0;b==B&&bookname[i]!='\0'&&st1[i]!='\0'&&(st1[i]==bookname[i]||st1[i]==bookname[i]+32);i++);
+                                        if(bookname[i]=='\0'&&st1[i]=='\0')
+                                            {
+                                            	
+                                                cont++;
+                                                getdata();
+                                                intf.seekp(intf.tellp()-sizeof(*this));
+                                                intf.write((char*)this,sizeof(*this));
+                                                break;
+                                                
+                                            }
+                                                     intf.read((char*)this,sizeof(*this));
+                                                     
+                                    }
+                                    
+                                    intf.close();
+                                    
+                            }
+                            
